cf4c17c4a50d5626eb01eb37c741644fed510251
simplysaadali <ali091g@gmail.com>
2025-12-07 15:33:20 +0500

    PF Quiz qith required output is done.

---
+++ PF-Quiz-1/photocopy_print_shop.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int printJobs, jobCost;
+    float totalAmount = 0;
+
+    cout << "Please enter number of print jobs you have done: ";
+    cin>>printJobs;
+
+    for(int i=1; i<=printJobs; i++){
+        int pagesToPrint;
+
+        cout << "Please enter pages printed for customer " << i << ": ";
+        cin >> pagesToPrint;
+
+        // as it is mentioned that cost of one page is 5.
+        jobCost = pagesToPrint * 5;
+
+        // applying 10% discount as mentioned if pages are more than 80.
+        if(pagesToPrint > 80){
+            jobCost = jobCost - (jobCost * 10/100);
+        }
+
+        cout << "Amount for job " << i << " is: " << jobCost << endl;
+        
+        totalAmount = totalAmount + jobCost;
+    }
+
+    cout << endl << "The total earnings done from all the customers are: " << totalAmount;
+    return 0;
+}
